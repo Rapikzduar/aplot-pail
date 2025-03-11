88ee10eaef94d613711e270b37b126a9b58dd8b6
Rapikzduar <rafiqmarwan80@gmail.com>
2025-03-11 07:08:30 +0700

    y

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post('/upload', async (req, res) => {
         File succes upload, rawURL result:
       </div>
       <div class="text-center">
-        <a id="rawUrlLink" href="${rawUrl}" class="text-blue-500 hover:text-blue-700">${rawUrl}</a>
+        ${rawUrl}
       </div>
       <div class="text-center mt-4">
         <button onclick="copyUrl()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Salin URL</button>
